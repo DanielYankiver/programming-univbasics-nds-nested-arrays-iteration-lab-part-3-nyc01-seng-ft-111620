8a691bf4bc0e21d7f3c90547779c9b566f151d29
Daniel Yankiver <dyankiver@gmail.com>
2020-11-05 22:37:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,18 +4,17 @@ require 'pry'
 # Combine all Strings present in the AoA into a single value and return it
   
 def join_nested_strings(src)
-  string_array = []
+  string = ""
   row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
-        string_array << src[row_index][element_index]
+        string << src[row_index][element_index]
         element_index += 1
-        binding.pry
       end
     end
     row_index += 1
   end
-  string_array
+  string
 end
